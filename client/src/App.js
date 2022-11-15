db23048364bc4ab62da4bcd36a79726153dd8fa1
Jingsi <gongjingsi@hotmail.com>
2022-11-14 21:55:33 -0500

    build the profile page

--- client/src/App.js
+++ client/src/App.js
@@ -42,12 +42,12 @@ function App() {
                 path="/signup"
                 element={<Signup />}
               />
+              <Route path="/profile">
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
               <Route
-                path="/profile"
-                element={<Profile />}
-              />
-              <Route
-                path="/thought"
+                path="/thought/:id"
                 element={<SingleThought />}
               />
 
